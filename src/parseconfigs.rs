100dcbf6aa035d62f79e30bab2b210c2528fb79d
Jonathan Kappler <kapplerjonathan4@gmail.com>
2022-10-21 12:59:29 +0200

    refactoring
    split code into multiple files and added comments. functionality unchanged.

---
+++ src/parseconfigs.rs
@@ -0,0 +1,122 @@
+use crate::sundata::get_sun_data_key_from_shortcut;
+use std::{fs, collections::HashMap};
+use chrono::{prelude::*, Duration};
+use ini::Ini;
+
+
+pub fn parse_cycle_online(filepath: &str, sun_data: HashMap<String, NaiveTime>) -> Vec<(NaiveTime, String)> {
+    let config = fs::read_to_string(filepath).unwrap().replace(" ", "");
+
+    // whole config => single entries
+    let config: Vec<&str> = config.split('\n').collect();
+
+    // entry => timestamp / vec of paths
+    let mut config_timestamped: Vec<(NaiveTime, Vec<&str>)> = Vec::new();
+    for tp in config {
+        // entry => timestr or shortcut / str of paths
+        let time_paths: Vec<&str> = tp.split('=').collect();
+
+        let time: NaiveTime;
+        // if shortcut
+        if time_paths[0].starts_with("#") {
+            // timestr or SHORTCUT => shortcut / defaulttimestr
+            let time_str: Vec<&str> = time_paths[0].split(':').collect();
+            let shortcut = time_str[0].get(1..).unwrap();
+            time = sun_data.get(get_sun_data_key_from_shortcut(shortcut)).unwrap().clone();
+        } else {
+            // TIMESTR or shortcut => hour / minute
+            let time_str: Vec<&str> = time_paths[0].split('_').collect();
+            time = NaiveTime::from_hms(time_str[0].parse().unwrap(), time_str[1].parse().unwrap(), 0);
+        }
+
+        // str of paths => vec of paths
+        let paths: Vec<&str> = time_paths[1].split(',').collect();
+        config_timestamped.push((time, paths));
+    }
+
+    // sort after timestamps
+    config_timestamped.sort();
+
+    // create interpolated timestamps for paths between one and another timestamp
+    let mut config_interpolated: Vec<(NaiveTime, String)> = Vec::new();
+    for i in 0..config_timestamped.len() {
+        let (time, paths) = &config_timestamped[i];
+
+        let interpolation_step = if i == config_timestamped.len() - 1 { 
+            ((config_timestamped[0].0 - *time) + Duration::minutes(1440)) / (config_timestamped.len() - 1) as i32
+        } else {
+            (config_timestamped[i+1].0 - *time) / (config_timestamped.len() - 1) as i32
+        };
+
+        for j in 0..paths.len() {
+            config_interpolated.push((*time + interpolation_step * j as i32, paths[j].to_string()));
+        }
+    }
+
+    config_interpolated
+}
+
+pub fn parse_cycle_offline(filepath: &str) -> Vec<(NaiveTime, String)> {
+    let config = fs::read_to_string(filepath).unwrap().replace(" ", "");
+
+    // whole config => single entries
+    let config: Vec<&str> = config.split('\n').collect();
+
+    // entry => timestamp / vec of paths
+    let mut config_timestamped: Vec<(NaiveTime, Vec<&str>)> = Vec::new();
+    for tp in config {
+        // entry => timestr or shortcut / str of paths
+        let time_paths: Vec<&str> = tp.split('=').collect();
+
+        let time: NaiveTime;
+
+        // timestr or shortcut-defaulttimestr => hour / minute
+        let time_str: Vec<&str> = if time_paths[0].starts_with("#") {
+            time_paths[0].split(':').collect::<Vec<&str>>()[1].split('_').collect()
+        } else {
+            time_paths[0].split('_').collect()
+        };
+
+        time = NaiveTime::from_hms(time_str[0].parse().unwrap(), time_str[1].parse().unwrap(), 0);
+
+        // str of paths => vec of paths
+        let paths: Vec<&str> = time_paths[1].split(',').collect();
+        config_timestamped.push((time, paths));
+    }
+
+    // sort after timestamps
+    config_timestamped.sort();
+
+    // create interpolated timestamps for paths between one and another timestamp
+    let mut config_interpolated: Vec<(NaiveTime, String)> = Vec::new();
+    for i in 0..config_timestamped.len() {
+        let (time, paths) = &config_timestamped[i];
+
+        let interpolation_step = if i == config_timestamped.len() - 1 { 
+            ((config_timestamped[0].0 - *time) + Duration::minutes(1440)) / (config_timestamped.len() - 1) as i32
+        } else {
+            (config_timestamped[i+1].0 - *time) / (config_timestamped.len() - 1) as i32
+        };
+
+        for j in 0..paths.len() {
+            config_interpolated.push((*time + interpolation_step * j as i32, paths[j].to_string()));
+        }
+    }
+
+    config_interpolated
+}
+
+
+pub struct AdditionalData {
+    pub lat: f32,
+    pub lng: f32,
+}
+
+pub fn parse_data(filepath: &str) -> AdditionalData {
+    let config_data = Ini::load_from_file(filepath).unwrap();
+    let config_general = config_data.section(Some("General")).unwrap();
+    let lat = config_general.get("latitude").unwrap().parse::<f32>().unwrap();
+    let lng = config_general.get("longitude").unwrap().parse::<f32>().unwrap();
+
+    AdditionalData { lat, lng }
+}
---
+++ src/sundata.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+use chrono::{prelude::*, Duration};
+use serde_json::{Map, Value};
+
+const SUN_DATA: &'static [&'static str] = &["civil_twilight_begin", "sunrise", "solar_noon", "sunset", "civil_twilight_end", "solar_midnight"];
+pub fn get_sun_data_key_from_shortcut(sc: &str) -> &str {
+    match sc {
+        "tb" => "civil_twilight_begin",
+        "sr" => "sunrise",
+        "sn" => "solar_noon",
+        "ss" => "sunset",
+        "te" => "civil_twilight_end",
+        "sm" => "solar_midnight",
+        _ => panic!("sun data shortcut is not recognized")
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct ApiData {
+    results: Map<String, Value>,
+    status: String,
+}
+
+pub fn get_sun_data(lat: f32, lng: f32) -> HashMap<String, NaiveTime> {
+
+    let res_string = reqwest::blocking::get(format!("https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}")).unwrap().text().unwrap();
+    let res: ApiData = serde_json::from_str(res_string.as_str()).unwrap();
+    let res = res.results;
+
+    // Value(String) => NaiveTime
+    let mut results: HashMap<String, NaiveTime> = HashMap::new();
+    for (k, v) in res {
+        if !SUN_DATA.contains(&k.as_str()) { continue };
+
+        let s = v.as_str().unwrap();
+
+        // timestr + AM or PM => timestr / AM or PM
+        let tmp: Vec<&str> = s.split(' ').collect();
+        let (time, pm) = (tmp[0], tmp[1]);
+        let pm = if pm == "PM" { true } else { false };
+
+        // timestr => hour / minute / second
+        let hms: Vec<&str> = time.split(':').collect();
+        let (h, m, s) = (hms[0].parse().unwrap(), hms[1].parse().unwrap(), hms[2].parse().unwrap());
+        let mut time = NaiveTime::from_hms(h, m, s);
+        if pm {
+            time += Duration::hours(12);
+        }
+
+        let timezone_offset = Local::now().offset().to_string();
+        // offset => offset-hour / offset-minute
+        let timezone_offset: Vec<&str> = timezone_offset.get(1..).unwrap().split(':').collect();
+        time += Duration::minutes(60 * timezone_offset[0].parse::<i64>().unwrap() + timezone_offset[1].parse::<i64>().unwrap());
+
+        results.insert(k, time);
+    }
+
+    // add solar_midnight (solar_noon + 12h)
+    results.insert("solar_midnight".to_string(), results.get("solar_noon").unwrap().clone() + Duration::hours(12));
+
+    results
+}
